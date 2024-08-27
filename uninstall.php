62050110e6250276e0ec5b9ee8c675d9ffb743e4
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-08-27 05:41:10 +0300

    Removed example uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,32 +1,22 @@
 <?php
-// Exit if accessed directly
+/**
+ * Uninstall Script for Mini Gallery Plugin
+ */
+
 if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit();
+    exit;
 }
 
-// Delete custom post type posts
-$mgwpp_soora_posts = get_posts(array(
+// Delete custom post type data
+$sowar = get_posts(array(
     'post_type' => 'mgwpp_soora',
     'numberposts' => -1,
-    'post_status' => 'any',
+    'post_status' => 'any'
 ));
 
-foreach ($mgwpp_soora_posts as $post) {
-    wp_delete_post($post->ID, true);
+foreach ($sowar as $gallery_image) {
+    wp_delete_post(intval($gallery_image->ID), true);
 }
 
-// Remove custom role
+// Delete custom role
 remove_role('marketing_team');
-
-// Delete any plugin options stored in the options table
-delete_option('mgwpp_some_option_name');
-
-// Delete custom database tables if any
-global $wpdb;
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mgwpp_custom_table");
-
-// Clean up any transients if used
-delete_transient('mgwpp_some_transient_name');
-
-// Flush rewrite rules to remove custom post type permalinks
-flush_rewrite_rules();

943a4e6b9ed13a7e5f05c8c84eee89134231d626
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-09-10 00:00:17 +0300

    Added new logic to hanle multiple and single uploads but still an error at 186

--- mini-gallery.php
+++ mini-gallery.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Mini Gallery
  * Description: A WordPress plugin to display a simple custom gallery.
@@ -10,13 +11,14 @@
 if (!defined('ABSPATH')) exit;
 
 // Unique prefix for all functions and hooks
-function mgwpp_register_post_type() {
+function mgwpp_register_post_type()
+{
     $args = array(
         'public' => true,
         'label' => 'Gallery Image',
         'description' => 'Manage your galleries here',
         'show_in_rest' => false,
-        'show_in_menu'=> false,
+        'show_in_menu' => false,
         'rest_base' => 'soora-api',
         'menu_icon' => 'dashicons-format-gallery',
         'has_archive' => true,
@@ -45,7 +47,8 @@ function mgwpp_register_post_type() {
 add_action('init', 'mgwpp_register_post_type');
 
 // Enqueue front-end scripts and styles
-function mgwpp_enqueue_assets() {
+function mgwpp_enqueue_assets()
+{
     // Register scripts and styles
     wp_register_script('mg-carousel', plugin_dir_url(__FILE__) . 'public/js/carousel.js', array(), '1.0', true);
     wp_register_style('mg-styles', plugin_dir_url(__FILE__) . 'public/css/styles.css', array(), '1.0');
@@ -59,7 +62,8 @@ function mgwpp_enqueue_assets() {
 add_action('wp_enqueue_scripts', 'mgwpp_enqueue_assets');
 
 // Enqueue admin assets
-function mgwpp_enqueue_admin_assets() {
+function mgwpp_enqueue_admin_assets()
+{
     // Register scripts and styles
     wp_register_script('mg-admin-carousel', plugin_dir_url(__FILE__) . 'public/admin/js/mg-scripts.js', array('jquery'), '1.0', true);
     wp_register_style('mg-admin-styles', plugin_dir_url(__FILE__) . 'public/admin/css/mg-styles.css', array(), '1.0');
@@ -71,7 +75,8 @@ function mgwpp_enqueue_admin_assets() {
 add_action('admin_enqueue_scripts', 'mgwpp_enqueue_admin_assets');
 
 // Activation & Deactivation Hooks
-function mgwpp_plugin_activate() {
+function mgwpp_plugin_activate()
+{
     mgwpp_register_post_type();
     mgwpp_add_marketing_team_role();
     mgwpp_capabilities();
@@ -79,7 +84,8 @@ function mgwpp_plugin_activate() {
 }
 register_activation_hook(__FILE__, 'mgwpp_plugin_activate');
 
-function mgwpp_plugin_deactivate() {
+function mgwpp_plugin_deactivate()
+{
     unregister_post_type('mgwpp_soora');
     remove_role('marketing_team');
     flush_rewrite_rules();
@@ -87,7 +93,8 @@ function mgwpp_plugin_deactivate() {
 register_deactivation_hook(__FILE__, 'mgwpp_plugin_deactivate');
 
 // Uninstall Hook
-function mgwpp_plugin_uninstall() {
+function mgwpp_plugin_uninstall()
+{
     $sowar = get_posts(array(
         'post_type' => 'mgwpp_soora',
         'numberposts' => -1,
@@ -101,7 +108,8 @@ function mgwpp_plugin_uninstall() {
 register_uninstall_hook(__FILE__, 'mgwpp_plugin_uninstall');
 
 // Roles
-function mgwpp_add_marketing_team_role() {
+function mgwpp_add_marketing_team_role()
+{
     if (get_role('marketing_team') === null) {
         add_role('marketing_team', 'Marketing Team', array(
             'read' => true,
@@ -127,7 +135,8 @@ function mgwpp_add_marketing_team_role() {
 add_action('init', 'mgwpp_add_marketing_team_role');
 
 // Capabilities
-function mgwpp_capabilities() {
+function mgwpp_capabilities()
+{
     $roles = ['administrator', 'marketing_team'];
     foreach ($roles as $role_name) {
         $role = get_role($role_name);
@@ -152,7 +161,8 @@ function mgwpp_capabilities() {
 add_action('admin_init', 'mgwpp_capabilities');
 
 // Admin Menu
-function mgwpp_menu() {
+function mgwpp_menu()
+{
     if (current_user_can('edit_mgwpp_sooras')) {
         add_menu_page('Add New Mini Gallery', 'Mini Gallery', 'edit_mgwpp_sooras', 'mini-gallery', 'mgwpp_plugin_page', 'dashicons-format-gallery', 6);
     }
@@ -160,7 +170,8 @@ function mgwpp_menu() {
 add_action('admin_menu', 'mgwpp_menu');
 
 // Handle File Uploads
-function mgwpp_upload() {
+function mgwpp_upload()
+{
     // Verify nonce for security
     if (!isset($_POST['mgwpp_upload_nonce']) || !wp_verify_nonce(sanitize_text_field(wp_unslash($_POST['mgwpp_upload_nonce'])), 'mgwpp_upload_nonce')) {
         wp_die(esc_html__('Security check failed', 'text-domain'));
@@ -170,7 +181,7 @@ function mgwpp_upload() {
     if (!empty($_FILES['sowar']) && !empty($_POST['image_title']) && !empty($_POST['gallery_type'])) {
         $title = sanitize_text_field(wp_unslash($_POST['image_title']));
         $gallery_type = sanitize_text_field(wp_unslash($_POST['gallery_type']));
-        
+
         // Create a new post for the gallery
         $post_id = wp_insert_post(array(
             'post_title'  => $title,
@@ -238,7 +249,7 @@ function mgwpp_upload() {
                 $file = array(
                     'name'     => wp_unslash(sanitize_file_name($files['name'])),
                     'type'     => wp_unslash(sanitize_mime_type($files['type'])),
-                    'tmp_name' => $files['tmp_name'],
+                    'tmp_name' => wp_unslash(sanitize_file_name($files['tmp_name'])),
                     'error'    => wp_unslash(intval($files['error'])),
                     'size'     => wp_unslash(intval($files['size']))
                 );
@@ -287,13 +298,14 @@ add_action('admin_post_mgwpp_upload', 'mgwpp_upload');
 
 
 // Handle Gallery Deletion
-function mgwpp_delete_gallery() {
+function mgwpp_delete_gallery()
+{
     if (!isset($_GET['gallery_id']) || !isset($_GET['_wpnonce']) || !wp_verify_nonce(sanitize_text_field(wp_unslash($_GET['_wpnonce'])), 'mgwpp_delete_gallery')) {
         wp_die('Security check failed');
     }
 
     $gallery_id = intval($_GET['gallery_id']);
-    
+
     if (!current_user_can('delete_mgwpp_soora', $gallery_id)) {
         wp_die('You do not have permission to delete this gallery');
     }
@@ -305,8 +317,9 @@ function mgwpp_delete_gallery() {
 add_action('admin_post_mgwpp_delete_gallery', 'mgwpp_delete_gallery');
 
 
-function mgwpp_plugin_page() {
-    ?>
+function mgwpp_plugin_page()
+{
+?>
     <h1><?php echo esc_html__('Mini Gallery', 'mini-gallery'); ?></h1>
 
     <!-- Form to upload new gallery images -->
@@ -344,7 +357,7 @@ function mgwpp_plugin_page() {
     ));
     if ($galleries) {
         foreach ($galleries as $gallery) {
-            ?>
+    ?>
             <div>
                 <h3><?php echo esc_html($gallery->post_title) . ' (ID: ' . esc_html($gallery->ID) . ')'; ?></h3>
                 <p><?php echo esc_html($gallery->post_content); ?></p>
@@ -368,12 +381,12 @@ function mgwpp_plugin_page() {
                 <p><a href="<?php echo esc_url($delete_url); ?>" class="button button-secondary"><?php echo esc_html__('Delete Gallery', 'mini-gallery'); ?></a></p>
             </div>
             <hr>
-            <?php
+        <?php
         }
     } else {
         ?>
         <p><?php echo esc_html__('No galleries found.', 'mini-gallery'); ?></p>
-        <?php
+<?php
     }
 }
 
@@ -392,7 +405,7 @@ function mgwpp_gallery_shortcode($atts)
         if (!$gallery_type) {
             $gallery_type = 'single_carousel'; // Fallback to default if not set
         }
-        
+
         $images_per_page = 6; // Number of images per page for multi-carousel
         $offset = ($paged - 1) * $images_per_page;
 
@@ -409,16 +422,14 @@ function mgwpp_gallery_shortcode($atts)
                 $output .= '</div>';
             } elseif ($gallery_type === 'multi_carousel') {
                 $output .= '<div id="mg-multi-carousel" class="mg-gallery multi-carousel" data-page="' . esc_attr($paged) . '">';
-                
+
                 // Slice images for current page
-                $images = array_slice($all_images, $offset, $images_per_page);  
+                $images = array_slice($all_images, $offset, $images_per_page);
                 foreach ($images as $image) {
                     $imgwpp_url = wp_get_attachment_image_src($image->ID, 'medium');
                     $output .= '<div class="mg-multi-carousel-slide"><img src="' . esc_url($imgwpp_url[0]) . '" alt="' . esc_attr($image->post_title) . '" loading="lazy"></div>';
                 }
                 $output .= '</div>';
-                
-            
             } elseif ($gallery_type === 'grid') {
                 $output .= '<div class="grid-layout">';
                 foreach ($all_images as $image) {
@@ -436,4 +447,3 @@ function mgwpp_gallery_shortcode($atts)
     return $output;
 }
 add_shortcode('mgwpp_gallery', 'mgwpp_gallery_shortcode');
-
